1d05cf3424f92bd536a6519495aa31bcc8ff30fb
phuongdn <phuong.do@ntq-solution.com.vn>
2018-10-18 17:26:03 +0700

    admin

--- DigitalShop/DigitalShop/DigitalShop/Areas/Admin/Controllers/AdminCategoryController.cs
+++ DigitalShop/DigitalShop/DigitalShop/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DigitalShop.Areas.Admin.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        [Area("Admin")]
+        [Authorize]
         public IActionResult Index()
         {
             return View();
